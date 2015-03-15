d72577505ea887a4e486caf26dceedaae76dc3db
Justin Stribling <jastribl@uwaterloo.ca>
2015-03-15 17:38:27 -0400

    Revert "Class Clean-up"
    This reverts commit 91658dbdcb0f1dd5cdc759e90e7ad1859be2fc9d.

--- samples/Fruit.h
+++ samples/Fruit.h
@@ -1,99 +1,32 @@
 #include "Point.h"
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+
+using namespace std;
 
 class Fruit:public sf::Sprite {
-private:
-	bool bomb = false, dead = false, split = false;
-	Point velocity;
-	double rotationSpeed, rotation;
-	int type;
 
 public:
-	Fruit(bool bombGiven, int typeGiven, sf::Texture& tex) {
-		bomb = bombGiven;
+	bool isBomb = false;
+	double  xSpeed, ySpeed, rSpeed, rotation;
+	bool dead = false, split = false;
+	int type;
+	Fruit(bool bomb, int typeGiven, sf::Texture& tex) {
+		isBomb = bomb;
 		type = typeGiven;
 		setPosition(rand() % 1366, 868);
-		velocity = Point((1 + rand() % 6) * ((getPosition().x > (1366 / 2)) ? -1 : 1), -18);
-		rotationSpeed = rand() % 5 * ((rand() % 2 == 1) ? -1 : 1);
-		rotation = 0.0;
+		xSpeed = (1 + rand() % 6) * ((getPosition().x > (1366 / 2)) ? -1 : 1);
+		ySpeed = -18;
+		rSpeed = rand() % 5 * ((rand() % 2 == 1) ? -1 : 1);
+		rotation = 0;
 		setTexture(tex);
-	}
-
-	bool isBomb() {
-		return bomb;
-	}
-
-	bool isDead() {
-		return dead;
-	}
-
-	bool isSplit() {
-		return split;
-	}
-
-	Point getVelocity() {
-		return velocity;
-	}
-
-	int getXVelocity() {
-		return velocity.getX();
-	}
-
-	int getYVelocity() {
-		return velocity.getY();
-	}
-
-	double getRotation() {
-		return rotation;
-	}
-
-	int getType() {
-		return type;
-	}
-
-	void setBomb(bool bombGiven) {
-		bomb = bombGiven;
-	}
-
-	void setDead(bool deadGiven) {
-		dead = deadGiven;
-	}
-
-	void setSplit(bool splitGiven) {
-		split = splitGiven;
-	}
-
-	void setVelocity(Point velocityGiven) {
-		velocity = velocityGiven;
-	}
-
-	void setVelocity(int xGiven, int yGiven) {
-		velocity.setX(xGiven);
-		velocity.setY(yGiven);
-	}
-
-	void setXVelocity(int xGiven) {
-		velocity.setX(xGiven);
-	}
-
-	void setYVelocity(int yGiven) {
-		velocity.setY(yGiven);
-
-	}
-
-	void setRotation(double rotationGiven) {
-		rotation = rotationGiven;
-	}
-
-	void setType(int typeGiven) {
-		type = typeGiven;
 	}
 
 	void fly() {
 		if (!dead) {
-			velocity.addY(0.2f);
-			rotation += rotationSpeed;
-			setPosition(getPosition().x + velocity.getX(), getPosition().y + velocity.getY());
+			ySpeed += 0.2f;
+			rotation += rSpeed;
+			setPosition(getPosition().x + xSpeed, getPosition().y + ySpeed);
 			setRotation(rotation);
 		}
 	}
--- samples/hello-myo.cpp
+++ samples/hello-myo.cpp
@@ -1,21 +1,30 @@
 #define _USE_MATH_DEFINES
-//#include <stdexcept>
-//#include <string>
-//#include <algorithm>
-//#include "Point.h"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+#include <string>
+#include <algorithm>
+#include "Point.h"
 #include "Fruit.h"
 #include <myo/myo.hpp>
-//#include <SFML/Graphics.hpp>
-//#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include <SFML/Audio.hpp>
 #include <sstream>
 
+
 using namespace std;
 
+int boundsSet = 0;
+int screenWidth = 1366, screenHeight = 768;
 bool gameRunning = false;
-int numFruit = 6, score = 0, level = 1, fruitsKilled = 0, fails = 0, screenWidth = 1366, screenHeight = 768, boundsSet = 0;
+int numFruit = 6;
 vector<Fruit*> fruit;
-double armX = 0, armY = 0, sordX = 1000, sordY = 1000, width, height, fruitTime = 2.0;
+double armX = 0, armY = 0, sordX = 1000, sordY = 1000;
+double width, height;
+double fruitTime = 2.0;
+int score = 0, level = 1, fruitsKilled = 0, fails = 0;
 
 class DataCollector: public myo::DeviceListener {
 public:
@@ -31,11 +40,11 @@ public:
 	}
 
 	void onOrientationData(myo::Myo* myo, uint64_t timestamp, const myo::Quaternion<float>& quat) {
-		//using std::atan2;
-		//using std::asin;
-		//using std::sqrt;
-		//using std::max;
-		//using std::min;
+		using std::atan2;
+		using std::asin;
+		using std::sqrt;
+		using std::max;
+		using std::min;
 
 		//float roll = atan2(2.0f * (quat.w() * quat.x() + quat.y() * quat.z()), 1.0f - 2.0f * (quat.x() * quat.x() + quat.y() * quat.y()));
 		float pitch = asin(max(-1.0f, min(1.0f, 2.0f * (quat.w() * quat.y() - quat.z() * quat.x()))));
@@ -104,14 +113,16 @@ public:
 
 	bool onArm;
 	myo::Arm whichArm;
+
 	bool isUnlocked;
+
 	int roll_r, pitch_y, yaw_x;
 	myo::Pose currentPose;
 };
 
 int main(int argc, char** argv) {
 	sf::RenderWindow* window(new sf::RenderWindow(sf::VideoMode(screenWidth, screenHeight), "Myo-Fruit", sf::Style::None));
-	window->setMouseCursorVisible(true);
+	window->setMouseCursorVisible(false);
 	sf::View view(sf::FloatRect(0, 0, screenWidth, screenHeight));
 	window->setView(view);
 	sf::Font font;
@@ -150,11 +161,9 @@ int main(int argc, char** argv) {
 		window->setFramerateLimit(0.f);
 		myo::Hub hub("com.justin.myo-fruit-ninja");
 		cout << "Attempting to find a Myo..." << endl;
-		myo::Myo* myo = hub.waitForMyo(1000);
-		if (!myo) {
-			cout << "Unable to find a Myo!" << endl;
-			return 0;
-		}
+		myo::Myo* myo = hub.waitForMyo(10000);
+		if (!myo)
+			throw runtime_error("Unable to find a Myo!");
 		cout << "Connected to a Myo armband!" << endl << endl;
 		myo->unlock(myo::Myo::unlockHold);
 		DataCollector collector;
@@ -171,8 +180,7 @@ int main(int argc, char** argv) {
 		}
 		hub.addListener(&collector);
 		while (window->isOpen()) {
-			if (myo)
-				hub.run(10);
+			hub.run(10);
 			sf::Event event;
 			while (window->pollEvent(event)) {
 				if (event.type == sf::Event::Closed)
@@ -200,8 +208,8 @@ int main(int argc, char** argv) {
 						bool isBomb = rand() % 5 == 1;
 						int type = (rand() % numFruit) * 3;
 						Fruit *newFruit = new Fruit(isBomb, type, isBomb ? bombTexture1 : (fruitTextures[type]));
-						newFruit->setSplit(false);
-						newFruit->setDead(false);
+						newFruit->split = false;
+						newFruit->dead = false;
 						newFruit->setOrigin(newFruit->getOrigin().x + 37.5, newFruit->getOrigin().y + 37.5);
 						fruit.push_back(newFruit);
 						clock.restart();
@@ -210,23 +218,23 @@ int main(int argc, char** argv) {
 					for (int i = 0; i < fruit.size(); i++) {
 						fruit[i]->fly();
 						for (int j = 0; j < pointers.size(); j++) {
-							if (pointers[j] && !fruit[i]->isDead() && !fruit[i]->isSplit() && abs(fruit[i]->getPosition().x - pointers[j]->getX()) < 37.5 && abs(fruit[i]->getPosition().y - pointers[j]->getY()) < 37.5) {
-								Fruit *newFruit = new Fruit(fruit[i]->isBomb(), fruit[i]->getType(), fruit[i]->isBomb() ? bombTexture2 : (fruitTextures[fruit[i]->getType() + 1]));
-								newFruit->setYVelocity(fruit[i]->getYVelocity() - 0.5);
+							if (pointers[j] && !fruit[i]->dead && !fruit[i]->split && abs(fruit[i]->getPosition().x - pointers[j]->x) < 37.5 && abs(fruit[i]->getPosition().y - pointers[j]->y) < 37.5) {
+								Fruit *newFruit = new Fruit(fruit[i]->isBomb, fruit[i]->type, fruit[i]->isBomb ? bombTexture2 : (fruitTextures[fruit[i]->type + 1]));
+								newFruit->ySpeed = fruit[i]->ySpeed - 0.5;
 								newFruit->setPosition(fruit[i]->getPosition());
 								newFruit->setOrigin(newFruit->getOrigin().x + 37.5, newFruit->getOrigin().y + 37.5);
-								newFruit->setSplit(true);
-								newFruit->setDead(false);
+								newFruit->split = true;
+								newFruit->dead = false;
 								fruit.push_back(newFruit);
-								Fruit *newFruit2 = new Fruit(fruit[i]->isBomb(), fruit[i]->getType(), fruit[i]->isBomb() ? bombTexture3 : (fruitTextures[fruit[i]->getType() + 2]));
-								newFruit2->setYVelocity(fruit[i]->getYVelocity() - 0.5);
+								Fruit *newFruit2 = new Fruit(fruit[i]->isBomb, fruit[i]->type, fruit[i]->isBomb ? bombTexture3 : (fruitTextures[fruit[i]->type + 2]));
+								newFruit2->ySpeed = fruit[i]->ySpeed - 0.5;
 								newFruit2->setPosition(fruit[i]->getPosition());
 								newFruit2->setOrigin(newFruit2->getOrigin().x + 37.5, newFruit2->getOrigin().y + 37.5);
-								newFruit2->setSplit(true);
-								newFruit2->setDead(false);
+								newFruit2->split = true;
+								newFruit2->dead = false;
 								fruit.push_back(newFruit2);
-								fruit[i]->setDead(true);
-								if (fruit[i]->isBomb()) {
+								fruit[i]->dead = true;
+								if (fruit[i]->isBomb) {
 									myo->vibrate(myo::Myo::vibrationShort);
 									bombSound.play();
 									fails++;
@@ -234,22 +242,22 @@ int main(int argc, char** argv) {
 								} else {
 									splashSound.play();
 									fruitsKilled++;
-									level = fruitsKilled / 5;
+									level = fruitsKilled % 10;
 									fruitTime = 2 - (level*0.1);
 									score += 50 * level;
 								}
 							}
 						}
-						if (!fruit[i]->isDead() && !fruit[i]->isSplit() && fruit[i]->getPosition().y > screenHeight && fruit[i]->getYVelocity() > 0) {
-							if (!fruit[i]->isBomb())
+						if (!fruit[i]->dead && !fruit[i]->split && fruit[i]->getPosition().y > screenHeight && fruit[i]->ySpeed > 0) {
+							if (!fruit[i]->isBomb)
 								fails++;
-							fruit[i]->setDead(true);
+							fruit[i]->dead = true;
 						}
 					}
 				}
 				if (gameRunning) {
 					for (int i = 0; i < fruit.size(); i++) {
-						if (fruit[i]->isDead()) {
+						if (fruit[i]->dead) {
 							swap(fruit[i], fruit.back());
 							fruit.pop_back();
 							cout << "fruit gone" << endl;
@@ -260,9 +268,9 @@ int main(int argc, char** argv) {
 					pointers.push_back(new Point(sordX, sordY));
 					for (int i = 1; i < pointers.size(); i++) {
 						if (pointers[i]) {
-							sf::Vertex line[] = {sf::Vertex(sf::Vector2f(pointers[i - 1]->getX(), pointers[i - 1]->getY())), sf::Vertex(sf::Vector2f(pointers[i]->getX(), pointers[i]->getY())), };
+							sf::Vertex line[] = {sf::Vertex(sf::Vector2f(pointers[i - 1]->x, pointers[i - 1]->y)), sf::Vertex(sf::Vector2f(pointers[i]->x, pointers[i]->y)), };
 							window->draw(line, 2, sf::Lines);
-							if (i > 25) {
+							if (i>25) {
 								pointers.erase(pointers.begin());
 							}
 						}
@@ -314,7 +322,6 @@ int main(int argc, char** argv) {
 				level = 1;
 				fruitsKilled = 0;
 				fails = 0;
-				fruitTime = 2.0;
 			}
 			window->setView(window->getDefaultView());
 			window->display();
